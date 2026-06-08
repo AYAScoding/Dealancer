e232c5cbb6e42e974ffb9762e1812a4e80609ef2
AYAScoding <169091190+AYAScoding@users.noreply.github.com>
2026-06-09 00:24:28 +0300

    Fix ALLOWED_HOSTS and DEBUG for Render

--- Backend/core/core/settings.py
+++ Backend/core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ja#7lo2q$jm-x)1roqv(i)40u=ar2oyjzx0env@beutb7lth#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CORS — allow the React dev server to talk to Django
 CORS_ALLOWED_ORIGINS = [
